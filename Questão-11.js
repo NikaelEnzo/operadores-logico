17a1f6b7872565f33807bd60d9328b1ba012780c
NikaelEnzo <enzo.marthins@gmail.com>
2024-03-25 17:05:46 -0400

    Questão 11 concluida

---
+++ Questão-11.js
@@ -0,0 +1,9 @@
+/*
+11. Explique a diferença entre os operadores && e || em JavaScript. 
+*/
+
+/*
+    && representa o "e" no java script, vai sempre priorizar resultados false, como true && false = false
+    enquanto o
+    || representa o "ou" no java script, vai sempre priorizar resultados true, como true || false = true
+*/
