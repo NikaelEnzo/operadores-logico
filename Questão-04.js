99fd50ee972a7330d167e5f5948be68efcc12b1b
NikaelEnzo <enzo.marthins@gmail.com>
2024-03-25 17:03:26 -0400

    Questão 04 concluida

---
+++ Questão-04.js
@@ -0,0 +1,7 @@
+/*
+4. Explique o que significa a expressão !(true && false). 
+*/
+
+/*                !(true && false)
+              Verdadeiro && Falso   resulta em Falso, mas a ! repsenta uma negação o que faz dar retorno Verdadeiro
+*/
