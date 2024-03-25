2aa733fb32d1622b68c2a684f7f337106546219a
NikaelEnzo <enzo.marthins@gmail.com>
2024-03-25 17:07:39 -0400

    Questão 17 concluida

---
+++ Questão-17.js
@@ -0,0 +1,8 @@
+/*
+17. Explique o que significa a expressão !(10 <= 5).
+*/
+
+/*
+    ! representa um "não" enquanto <= diz que 10 e menor ou igual a 5
+    juntando tudo ele diz "10 não é menor ou igual a 5"
+*/
