41bed5c9c32098cfbf8a7f14f705f31df3dc65f3
NikaelEnzo <enzo.marthins@gmail.com>
2024-03-25 17:01:27 -0400

    Questão 01 concluida

---
+++ Questão-01.js
@@ -0,0 +1,10 @@
+/*
+1. O resultado de (10 > 5) && (7 < 9) é verdadeiro ou falso?
+*/
+
+/*                (10 > 5) && (7 < 9)
+                    true   &&   true  = true
+
+primeiro &&
+depois ||
+*/
