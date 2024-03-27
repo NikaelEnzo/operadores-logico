b04f7293c87499f1a0e2c78ed423b5c5048986f3
NikaelEnzo <enzo.marthins@gmail.com>
2024-03-27 14:35:43 -0400

    Questão 26 concluida

---
+++ Questão-26.js
@@ -0,0 +1,8 @@
+/*
+26. Explique como os parênteses afetam o resultado de uma expressão com 
+operadores lógicos em JavaScript.
+*/
+
+/*
+    os parênteses () representam uma prioridade na resolução da expressão lógica
+*/
