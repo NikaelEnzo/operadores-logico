228fc179c4ffa24bd828a82104aa719e50bbf6c1
NikaelEnzo <enzo.marthins@gmail.com>
2024-03-27 13:53:13 -0400

    Questão 21 concluida

---
+++ Questão-21.js
@@ -0,0 +1,12 @@
+/*
+21. Explique como você pode usar operadores lógicos para verificar se uma 
+pessoa tem idade entre 18 e 65 anos em JavaScript. 
+*/
+
+let idade = 30
+
+if (idade >= 18 && idade <=65) {
+    console.log("Idade entre 18 e 65 anos")
+} else {
+    console.log("Não esta entre 18 e 65 anos")
+}
